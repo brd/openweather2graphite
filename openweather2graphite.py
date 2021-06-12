83d8edd2cf04a2b1fb0551eeeddf05c8f27cd7ad
Brad Davis <brd@FreeBSD.org>
2021-06-12 13:09:50 -0600

    Add snow and support for other zip codes

--- openweather2graphite.py
+++ openweather2graphite.py
@@ -19,37 +19,58 @@ def publish_to_carbon(config, path, value):
     print(f'Error: {str(e)}')
 
 
-def poll_openweather_api(config):
+def poll_openweather_api(config, zip):
   # api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
   payload = {
-    "zip": config["zip_code"],
+    "zip": zip,
     "appid": config["openweather_key"],
     "units": config["units"]
   }
-  r = requests.get(OPENWEATHER_URL, params=payload)
   try:
+    r = requests.get(OPENWEATHER_URL, params=payload)
     if r.status_code == requests.codes.ok:
       res = None
       res = r.json()
 
-    if "main" in res:
-      for x in ['temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity' ]:
-        second = "temperature"
-        if x == "humidity":
-          second = "humidity"
-        if x == 'pressure':
-          second = "pressure"
+      if "main" in res:
+        for x in ['temp', 'feels_like', 'temp_min', 'temp_max', 'pressure', 'humidity' ]:
+          second = "temperature"
+          if x == "humidity":
+            second = "humidity"
+          if x == 'pressure':
+            second = "pressure"
 
-        publish_to_carbon(config, 'house/' + second + '/openweather_' + x, res['main'][x])
-        # The OpenWeather API only updates every 10 minutes, so fill in
-        # the gaps in the graph with the same value every 60 seconds for
-        # the rest of the 10 minute period
-        for t in range(60, 600, 60):
-          config['s'].enter(t, 1, publish_to_carbon, argument=(config, 'house/' + second + '/openweather_' + x, res['main'][x]))
-    if "clouds" in res:
-      publish_to_carbon(config, 'house/clouds/openweather_clouds', res['clouds']['all'])
-      for t in range(60, 600, 60):
-        config['s'].enter(t, 1, publish_to_carbon, argument=(config, 'house/clouds/openweather_clouds', res['clouds']['all']))
+          if zip != 80020:
+            publish_to_carbon(config, 'weather/' + str(zip) + '/openweather_' + x, res['main'][x])
+          else:
+            publish_to_carbon(config, 'house/' + second + '/openweather_' + x, res['main'][x])
+          # The OpenWeather API only updates every 10 minutes, so fill in
+          # the gaps in the graph with the same value every 60 seconds for
+          # the rest of the 10 minute period
+          if zip != 80020:
+            config['s'].enter(60, 1, publish_to_carbon, argument=(config, 'weather/' + str(zip) + '/openweather_' + x, res['main'][x]))
+            continue
+          else:
+            config['s'].enter(60, 1, publish_to_carbon, argument=(config, 'house/' + second + '/openweather_' + x, res['main'][x]))
+
+      if "clouds" in res:
+        if zip != 80020:
+          publish_to_carbon(config, 'weather/' + str(zip) + '/openweather_clouds', res['clouds']['all'])
+          config['s'].enter(60, 1, publish_to_carbon, argument=(config, 'weather/' + str(zip) + '/openweather_clouds', res['clouds']['all']))
+        else:
+          publish_to_carbon(config, 'house/clouds/openweather_clouds', res['clouds']['all'])
+          config['s'].enter(60, 1, publish_to_carbon, argument=(config, 'house/clouds/openweather_clouds', res['clouds']['all']))
+      # rain/snow
+      for x in ['rain', 'snow']:
+        if x in res:
+          print(f'x: {x}')
+          for y in ['1h', '3h']:
+            if zip != 80020:
+              publish_to_carbon(config, 'weather/' + str(zip) + '/openweather_' + x + '_' + y, res[x][y])
+              config['s'].enter(60, 1, publish_to_carbon, argument=(config, 'weather/' + str(zip) + '/openweather_' + x + '_' + y, res[x][y]))
+            else:
+              publish_to_carbon(config, 'house/' + x + '/openweather_' + x + '_' + y, res[x][y])
+              config['s'].enter(60, 1, publish_to_carbon, argument=(config, 'house/' + x + '/openweather_' + x + '_' + y, res[x][y]))
 
   except Exception as e:
     print(f'Error: {str(e)}')
@@ -68,8 +89,12 @@ def read_config():
 
 
 def schedule_next(config):
-  config['s'].enter(600, 1, schedule_next, argument=(config,))
-  poll_openweather_api(config)
+  config['s'].enter(120, 1, schedule_next, argument=(config,))
+  if isinstance(config["zip_code"], list):
+    for zip in config["zip_code"]:
+      poll_openweather_api(config, zip)
+  else:
+    poll_openweather_api(config, config["zip_code"])
 
 
 def main():
